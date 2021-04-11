454474f90b92a78d5b5f2a57994a90b80529d641
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2021-04-11 20:02:33 +0300

    modified main.html (former index.html) and app.py

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask
-from flask import redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import redirect, render_template, request, session
+#from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
-def index():
-	return render_template("index.html")
+def main():
+	return render_template("main.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-	return render_template("login.html")
+	username= request.form["username"]
+	password= request.form["password"]
+	session["username"] = username
+	return redirect("/")
+
+@app.route("/logout")
+def logout():
+	del session["username"]
+	return redirect("/")
+
